71965a616769246ff2180ef0cd7f5547e11f01b5
nickcampoli <dacuel21@gmail.com>
2019-09-26 16:46:17 -0500

    Started Manager

---
+++ Assets/Src/Player/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager Instance { get; private set; }
+
+    public int Score;
+    public int Moves;
+    public float Time;
+    public int Deaths;
+    
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
